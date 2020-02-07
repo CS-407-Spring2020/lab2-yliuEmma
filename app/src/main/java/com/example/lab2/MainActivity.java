655a4f0c6cf2e84b78997e151c91ef9411950780
Emma Liu <liu763@wisc.edu>
2020-02-07 15:07:07 -0600

    created a button and a function for it

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.lab2;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.util.Log;
+import android.view.View;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void buttonClicked (View view){
+        Log.i("Info", "Button CLicked!");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

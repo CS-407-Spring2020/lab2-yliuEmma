8023c65bbcf62f2700c8d310e9292329e1f5fa17
Emma Liu <liu763@wisc.edu>
2020-02-07 15:35:40 -0600

    added plain text and toast

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -4,10 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
     public void buttonClicked (View view){
+        EditText myTextField = (EditText) findViewById(R.id.editText);
+        Toast.makeText(MainActivity.this, myTextField.getText().toString(), Toast.LENGTH_LONG).show();
+
         Log.i("Info", "Button CLicked!");
     }
 

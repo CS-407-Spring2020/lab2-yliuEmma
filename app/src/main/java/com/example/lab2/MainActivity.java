04a0594a79eeaacc07287b98a8544b5a7deb1bfb
Emma Liu <liu763@wisc.edu>
2020-02-10 15:08:01 -0600

    first milestone done

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.lab2;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.util.Log;
 import android.view.View;
 import android.os.Bundle;
@@ -8,13 +10,23 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 
+
 public class MainActivity extends AppCompatActivity {
 
     public void buttonClicked (View view){
         EditText myTextField = (EditText) findViewById(R.id.editText);
+        String str = myTextField.getText().toString();
         // Toast.makeText(MainActivity.this, myTextField.getText().toString(), Toast.LENGTH_LONG).show();
 
         Log.i("Info", "Button CLicked!");
+
+        goToActivity2(str);
+    }
+
+    public void goToActivity2 (String s) {
+        Intent intent = new Intent (this, Main2Activity.class);
+        intent.putExtra("message", s);
+        startActivity(intent);
     }
 
     @Override
